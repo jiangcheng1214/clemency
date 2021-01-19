4fa41d503102bc56a292210d2e80d76d7f805599
Cheng Jiang <jiangcheng1214@gmail.com>
2021-01-18 23:38:05 -0800

    codebase name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'IQapp'`, () => {
+  it(`should have as title 'clemency'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IQapp');
+    expect(app.title).toEqual('clemency');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('IQapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('clemency app is running!');
   });
 });
