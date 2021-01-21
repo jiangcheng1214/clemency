6cd8d1ef613aabe3b4cb7042a375e503e7d5ae3b
Cheng Jiang <jiangcheng1214@gmail.com>
2021-01-20 22:32:43 -0800

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBBve_jB0q05Opq6arVmg2pd_v4xFMGw5g",
+    authDomain: "clemency-firebase-000.firebaseapp.com",
+    databaseURL: "https://clemency-firebase-000-default-rtdb.firebaseio.com",
+    projectId: "clemency-firebase-000",
+    storageBucket: "clemency-firebase-000.appspot.com",
+    messagingSenderId: "1060237460978",
+    appId: "1:1060237460978:web:1f3d35481efca7d3d2176b",
+    measurementId: "G-KXZM0B46LL"
+  }
 };
