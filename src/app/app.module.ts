dd639aa62195597d37fb0dd27d6ea54a5d32160d
Cheng Jiang <jiangcheng1214@gmail.com>
2021-01-20 22:22:00 -0800

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { IQTestComponent } from './components/main/iqtest/iqtest.component';
 import { DeclarationComponent } from './components/main/declaration/declaration.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,9 @@ import { DeclarationComponent } from './components/main/declaration/declaration.
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBBve_jB0q05Opq6arVmg2pd_v4xFMGw5g",
+    authDomain: "clemency-firebase-000.firebaseapp.com",
+    databaseURL: "https://clemency-firebase-000-default-rtdb.firebaseio.com",
+    projectId: "clemency-firebase-000",
+    storageBucket: "clemency-firebase-000.appspot.com",
+    messagingSenderId: "1060237460978",
+    appId: "1:1060237460978:web:1f3d35481efca7d3d2176b",
+    measurementId: "G-KXZM0B46LL"
+  }
 };
 
 /*
