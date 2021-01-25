3f9d143ff1aa3e61b269e7a039b1427ae979dd42
Cheng Jiang <jiangcheng1214@gmail.com>
2021-01-24 23:39:23 -0800

    update static upload path

--- src/app/components/main/iqtest/iqtest.component.ts
+++ src/app/components/main/iqtest/iqtest.component.ts
@@ -6,14 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iqtest.component.css']
 })
 export class IQTestComponent implements OnInit {
-
+  testStarted:boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.testStarted = false;
   }
 
   onTestStart(): void {
-
+    console.log("start test clicked")
+    this.testStarted = true;
   }
 
 }
