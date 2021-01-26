7ccf2ae544fe46dec3a513b414a9b21726c982db
Cheng Jiang <jiangcheng1214@gmail.com>
2021-01-26 01:02:22 -0800

    update

---
+++ src/app/modules/localization/localization.module.ts
@@ -0,0 +1,112 @@
+export namespace LocalizationConstants {
+    export class APP {
+        public static TITLE = new Map([
+            ['en', 'International IQ test'],
+            ['ch', '国际标准IQ测试'],
+        ]);
+        public static TAKE_TEST_TITLE = new Map([
+            ['en', 'Take IQ test'],
+            ['ch', '智商测试'],
+        ]);
+        public static RECOVER_RESULT_TITLE = new Map([
+            ['en', 'Recover my result'],
+            ['ch', '找回我的结果'],
+        ]);
+    }
+    export class LiveResults {
+        public static DESCRIPTION = new Map([
+            ['en', 'Live Results'],
+            ['ch', '最近结果'],
+        ]);
+    }
+    export class IQTest {
+        public static TITLE = new Map([
+            ['en', 'International IQ test'],
+            ['ch', '国际标准IQ测试'],
+        ]);
+        public static INTRODUCTION = new Map([
+            ['en', 'Welcome to the international IQ test.\n\
+            Before starting the test, make sure you are in a place where you can focus.\n\
+            We will evaluate, through 40 questions, your ability to learn, to understand, \
+            to form concepts, to process information, and to apply logic and reason.\n\
+            Your result at the end of this test will inform you of your IQ as well as your \
+            position in comparison to the population using several statistics.'],
+            ['ch', '欢迎进行国际标准IQ测试。\n在测试开始之前，确保您所在的地方能使您的注意力集中。\n\
+            我们将通过40个问题，旨在评估您的学习，理解，概念形成，信息处理，应用逻辑和理由方面的能力。\n\
+            测试结束后，您将得到您的IQ 的结果, 以及此结果在整个测试使用人群的比例。'],
+        ]);
+        public static BUTTON_TEXT = new Map([
+            ['en', 'Start the test'],
+            ['ch', '开始测试'],
+        ]);
+    }
+    export class DECLARATION {
+        public static PURPOSE_TITLE = new Map([
+            ['en', 'The purpose of the IQ test'],
+            ['ch', 'IQ测试的目的'],
+        ]);
+        public static PURPOSE_TEXT = new Map([
+            ['en', 'The IQ test can be used for many reasons. It allows determining one\'s\
+             level of being able to learn, to understand, to form concepts, to process information,\
+              to apply logic and reason, in comparison to the population. It is indicative and can \
+              represent an influence on non-common characters that can be encountered in society, \
+              whether relational such as isolation, rejection or distance, or intellectual such as \
+              ease or difficulties encountered in intellectual activities. In another way, it can \
+              also explain successes acquired in ways easier than others by individuals.'],
+            ['ch', '进行IQ测试的原因有很多种。它能够决定一个人的学习，理解，概念形成，信息处理，应用逻辑和理由等方面\
+            相比较其他人群的水平。它能够指明社会中遇到的非主流人物的影响，是否存在隔离，拒绝或距离等关系，或是在活动中遇\
+            到的容易或困难的知识分子。 换句话说，它也可以解释一个人获得成功的方式比其他人更容易的原因。'],
+        ]);
+        public static EFFECTIVENESS_TITLE = new Map([
+            ['en', 'The effectiveness of this test'],
+            ['ch', '测试的有效性'],
+        ]);
+        public static EFFECTIVENESS_TEXT = new Map([
+            ['en', 'The IQ test we offer is used internationally by millions of users and new users take\
+            it every day. The statistics provided and established by the totality of the results make \
+            it possible to verify its reliability in relation to the calibration IQ scale system \
+            represented by the Gauss Curve.'],
+            ['ch', '我们提出的IQ测试在国际上被数百万用户使用，每天都有新用户在使用。 通过全部结果提供和建立的统计数据与高\
+            斯曲线IQ校正衡量系统的相关性，可验证其可靠性。'],
+        ]);
+        public static GIFTED_TITLE = new Map([
+            ['en', 'The gifted'],
+            ['ch', '天才'],
+        ]);
+        public static GIFTED_TEXT = new Map([
+            ['en', "People with much higher than average intellectual quotients (> 130), also called ‘gifted’, tend to be more at ease than others during intellectual activities. The most well-known characteristics of the gifted are:\n\
+            \tCuriosity and a thirst for learning: they ask themselves a lot of questions and are able to acquire knowledge on their own.\n\
+            \tPerfectionism, the need to do things accurately and excellently.\n\
+            \tThe fear of oneself, of who one is, of the consequences of their overflowing thoughts and emotions.\n\
+            \tInterest that sometimes reaches an obsessive level in certain themes.\n\
+            \tHypersensitivity that is often invisible from the outside.\n\
+            \tA great ability to pay attention and concentrate.\n\
+            \tMeta-cognitive awareness: they know how to identify and reuse plans, concepts and strategies that they can employ to solve problems."],
+            ['ch', '智商高于平均水平（> 130）的人，也被称为“天才”，在智力活动中往往比其他人表现得更容易。 最有名的天才特征是：\n\
+            \t好奇心和对学习的渴望：他们问自己很多问题，并且能够自己获得知识。\n\
+            \t完美主义，做事必须要准确和出色。\n\
+            \t对自己的恐惧，这是他们有着过度的思绪和情感的结果。\n\
+            \t在某些主题上兴趣极高，甚至达到有强迫症的程度。\n\
+            \t外表看不出有高度灵敏。\n\
+            \t有极高的集中注意力的能力。\n\
+            \t元认知意识：他们知道如何识别和重新使用计划，概念和策略，以便于去解决问题。']
+        ])
+        public static MENTAL_HANDICAP_TITLE = new Map([
+            ['en', 'The mental handicap'],
+            ['ch', '心理障碍'],
+        ]);
+        public static MENTAL_HANDICAP_TEXT = new Map([
+            ['en', 'People with very low intellectual quotients (<70) are defined as holders of a mental \
+            disability disorder and may experience, despite the intensity of work, more difficulty than \
+            others in intellectual activities and may also maintain, in spite of themselves, difficulties \
+            in social behaviours despite the strength of their will. The main causes that may explain the \
+            development of this disability would be nutritional problems during pregnancy or birth problems, \
+            such as a lack of oxygen. Exposure to certain diseases, such as whooping cough or measles, with \
+            delayed care could also explain the mental disability.'],
+            ['ch', '智商低（<70）的人被定义为有心理失能紊乱症，即使工作强度较大，但在智力活动中这类群体比其他人感觉更难，尽管\
+            他们有自己的意志，但在维持社交行为方面会遇到困难。这种失能主要的诱因可以解释成孕期的营养问题或是出生时缺氧所造成的。\
+            感染某些疾病，如百日咳或过晚诊治的麻疹，也可以引起心理失能紊乱症。'],
+        ]);
+        
+    }
+}
--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
-  selectedLanguage: String;
-  constructor() { 
-    this.selectedLanguage = 'en'
-  }
-  
-  updateSelectedLanguage(language:String) {
-    console.log("selectedLanguage changed to " + language)
-    this.selectedLanguage = language;
+  currentLanguageCodeSubject: Subject<String>;
+  public currentLanguageCode: String;
+
+  constructor(private router: Router,) {
+    this.currentLanguageCodeSubject = new Subject();
+    this.currentLanguageCode = 'en';
   }
 
-  getSelectedLanguage():String {
-    return this.selectedLanguage;
+  updateSelectedLanguageCode(languageCode: String) {
+    console.log("[LanguageService] selectedLanguage changed to " + languageCode)
+    this.currentLanguageCode = languageCode;
+    this.currentLanguageCodeSubject.next(languageCode)
+    this.router.navigateByUrl("/" + languageCode + "/")
   }
-  
+
+  languageSubject(): Subject<String> {
+    return this.currentLanguageCodeSubject;
+  }
+
 }
